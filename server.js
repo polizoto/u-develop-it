366f6d27173e17348c7349746089ad81d903e016
polizoto <polizoto@hotmail.com>
2022-01-29 15:19:18 -0500

    added queries to database

--- server.js
+++ server.js
@@ -1,12 +1,52 @@
 const express = require('express');
 
+const mysql = require('mysql2');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'root-password',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
 
+  // db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+  //   if (err) {
+  //     console.log(err);
+  //   }
+  //   console.log(row);
+  // });
+
+// Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+              VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result);
+});
+
+// // Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
